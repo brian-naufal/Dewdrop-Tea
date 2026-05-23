fd9c2f2810de932999750a4bb07cea212a711540
Bryan <bnaufalathailla12@gmail.com>
2026-05-23 13:44:24 +0700

    feat: initial setup

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
   // Example: simple console log to confirm the file loads
   console.log('Custom JS loaded');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarPlaceholder = document.getElementById("navbar-placeholder");
+
+    if (navbarPlaceholder) {
+        fetch("src/components/navbar.html")
+            .then(response => response.text())
+            .then(data => {
+                navbarPlaceholder.innerHTML = data;
+            })
+            .catch(error => console.error("Error loading navbar:", error));
+    }
+});
