072b82d03e6e45501857d97971b4f28e73633222
Bryan <bnaufalathailla12@gmail.com>
2026-05-23 16:42:27 +0700

    feat: implements home page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
-// Your custom JavaScript. Use this file to add interactivity.
 document.addEventListener('DOMContentLoaded', function () {
-  // Example: simple console log to confirm the file loads
   console.log('Custom JS loaded');
 });
 
@@ -14,5 +12,52 @@ document.addEventListener("DOMContentLoaded", function() {
                 navbarPlaceholder.innerHTML = data;
             })
             .catch(error => console.error("Error loading navbar:", error));
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarPlaceholder = document.getElementById("navbar-placeholder");
+
+    if (navbarPlaceholder) {
+        fetch("src/components/navbar.html")
+            .then(response => response.text())
+            .then(data => {
+                navbarPlaceholder.innerHTML = data;
+                let currentUrl = window.location.pathname;
+
+                if (currentUrl.endsWith("/")) {
+                    currentUrl += "index.html";
+                }
+
+                const navLinks = document.querySelectorAll('.nav-link');
+                navLinks.forEach(link => {
+
+                    if (link.href.includes(currentUrl)) {
+                        link.classList.add('active-link');
+                        link.classList.remove('text-dark');
+                    }
+                });
+            })
+            .catch(error => console.error("Error loading navbar:", error));
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // --- 1. NAVBAR INJECTION ---
+    const navbarPlaceholder = document.getElementById("navbar-placeholder");
+    if (navbarPlaceholder) {
+        fetch("src/components/navbar.html")
+    }
+
+    // --- 2. FOOTER INJECTION ---
+    const footerPlaceholder = document.getElementById("footer-placeholder");
+    if (footerPlaceholder) {
+        fetch("src/components/footer.html")
+            .then(response => response.text())
+            .then(data => {
+                footerPlaceholder.innerHTML = data;
+            })
+            .catch(error => console.error("Error loading footer:", error));
     }
 });
